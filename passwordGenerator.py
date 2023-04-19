ef989931ea1c90005e51a0f51a224b1b29fa7410
João Pedro <joaopedroicm2117@gmail.com>
2023-04-19 00:00:40 -0300

    V 1.2

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,16 +1,23 @@
+#Version 1.2
 import random
 
 letras_minusculas = chr(random.randint(97,122))
 letras_maiusculas = chr(random.randint(65,90))
 char_especial =chr(random.randint(33,64))
 lista_numeros = []
+senha_lista = []
 
 for i in range(6):
     numeros = random.randrange(9)
     lista_numeros.append(numeros)
 
 random.shuffle(lista_numeros)
-lista_numeros = str(lista_numeros).strip('[]')
-lista_numeros = lista_numeros.replace(',','')
+senha_lista = letras_maiusculas, letras_minusculas,char_especial
+senha_lista = list(senha_lista)
+senha_lista.extend(lista_numeros)
+random.shuffle(senha_lista)
 
-print(letras_maiusculas, letras_minusculas, lista_numeros, char_especial)
+senha_lista = str(senha_lista).strip('[]')
+senha_lista = senha_lista.replace(',','')
+
+print(senha_lista)
